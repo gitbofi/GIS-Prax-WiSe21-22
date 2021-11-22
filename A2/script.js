99962757a77f72a9f9f2f8ad635c43ea27a55226
Bofi <84191696+gitbofi@users.noreply.github.com>
2021-11-22 22:22:16 +0100

    Add files via upload

---
+++ A2/script.js
@@ -0,0 +1,81 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["artist"] = document.getElementById("artist").value;
+    formData["price"] = document.getElementById("price").value;
+    formData["location"] = document.getElementById("location").value;
+    formData["date"] = document.getElementById("date").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("event-list").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.artist;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.price;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.location;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.date;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("artist").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("location").value = "";
+    document.getElementById("date").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("artist").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("price").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("location").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("date").value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.artist;
+    selectedRow.cells[1].innerHTML = formData.price;
+    selectedRow.cells[2].innerHTML = formData.location;
+    selectedRow.cells[3].innerHTML = formData.date;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this event?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("event-list").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById("artist").value == "") {
+        isValid = false;
+        document.getElementById("artistValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("artistValidationError").classList.contains("hide"))
+            document.getElementById("artistValidationError").classList.add("hide");
+    }
+    return isValid;
+}
